73b8aa1a8679364a3a257e71e3460fba7891dab1
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-01-28 00:18:34 +0700

    Added the capability to add flags to the regex.

--- use-cases/process-message.js
+++ use-cases/process-message.js
@@ -4,10 +4,16 @@ module.exports.replyToMessage = async (groupChatId, message) => {
   let chatConfig = await Models.GroupChatConfig.findOne({ groupChatId: groupChatId }).exec();
 
   if (chatConfig.configs.length !== 0) {
-    let configIndex = chatConfig.configs.findIndex(config => (new RegExp(config.regex)).test(message));
+    const stringToRegex = (string) => {
+      var match = /^\/(.*)\/([a-z]*)$/.exec(string);
+      return new RegExp(match[1], match[2]);
+    };
+
+    let configIndex = chatConfig.configs.findIndex(config => stringToRegex(config.regex).test(message));
     
     if (configIndex !== -1) {
       console.log(`Replied to \'${message}\' with \'${chatConfig.configs[configIndex].reply}\'.`);
+      
       return chatConfig.configs[configIndex].reply;
     } else {
       return null;
