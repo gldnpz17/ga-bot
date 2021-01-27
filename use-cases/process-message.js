265976413d312e0991695c9c02a2a0be8dba0184
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-01-27 23:54:11 +0700

    Removed troblesome import.

--- use-cases/process-message.js
+++ use-cases/process-message.js
@@ -1,11 +1,10 @@
-const { config } = require('dotenv/types');
 const Models = require('../models/models');
 
 module.exports.replyToMessage = async (groupChatId, message) => {
   let chatConfig = await Models.GroupChatConfig.findOne({ groupChatId: groupChatId }).exec();
 
   if (chatConfig.configs.length !== 0) {
-    let configIndex = chatConfig.configs.findIndex(config => (new RegExp(config.regex)).test(message))
+    let configIndex = chatConfig.configs.findIndex(config => (new RegExp(config.regex)).test(message));
     
     if (configIndex !== -1) {
       console.log(`Replied to \'${message}\' with \'${chatConfig.configs[configIndex]}\'.`);
