85d758f7a839e16843111e58916aa1b5db226f68
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-01-27 23:45:31 +0700

    Hopefully this solves the message reply bug.

--- use-cases/process-message.js
+++ use-cases/process-message.js
@@ -1,18 +1,21 @@
+const { config } = require('dotenv/types');
 const Models = require('../models/models');
 
 module.exports.replyToMessage = async (groupChatId, message) => {
   let chatConfig = await Models.GroupChatConfig.findOne({ groupChatId: groupChatId }).exec();
 
   if (chatConfig.configs.length !== 0) {
-    chatConfig.configs.map(config => {
+    let configIndex = chatConfig.configs.findIndex(config => {
       let regex = new RegExp(config.regex);
 
       if (regex.test(message)) {
-        console.log(`Replied to \'${message}\' with \'${config.reply}\'.`)
+        console.log(`Replied to \'${message}\' with \'${config.reply}\'.`);
 
-        return config.reply;
+        return true;
       }
     });
+
+    return (configIndex === -1) ? chatConfig.configs[configIndex] : null;
   } else {
     return null;
   }
