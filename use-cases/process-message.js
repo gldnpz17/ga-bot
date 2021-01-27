e885dac6b88bdc7be4bea0f6e879165c21dfdf2a
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-01-27 23:55:58 +0700

    Yet another bug fix. I seriously need to learn dependency inversion in javascript.

--- use-cases/process-message.js
+++ use-cases/process-message.js
@@ -7,8 +7,8 @@ module.exports.replyToMessage = async (groupChatId, message) => {
     let configIndex = chatConfig.configs.findIndex(config => (new RegExp(config.regex)).test(message));
     
     if (configIndex !== -1) {
-      console.log(`Replied to \'${message}\' with \'${chatConfig.configs[configIndex]}\'.`);
-      return chatConfig.configs[configIndex];
+      console.log(`Replied to \'${message}\' with \'${chatConfig.configs[configIndex].reply}\'.`);
+      return chatConfig.configs[configIndex].reply;
     } else {
       return null;
     }
