d1f9b97fec9e192702ff22f7ff893a61b1e66a6b
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-01-27 23:51:44 +0700

    More bug fixes.

--- use-cases/process-message.js
+++ use-cases/process-message.js
@@ -5,17 +5,14 @@ module.exports.replyToMessage = async (groupChatId, message) => {
   let chatConfig = await Models.GroupChatConfig.findOne({ groupChatId: groupChatId }).exec();
 
   if (chatConfig.configs.length !== 0) {
-    let configIndex = chatConfig.configs.findIndex(config => {
-      let regex = new RegExp(config.regex);
-
-      if (regex.test(message)) {
-        console.log(`Replied to \'${message}\' with \'${config.reply}\'.`);
-
-        return true;
-      }
-    });
-
-    return (configIndex === -1) ? chatConfig.configs[configIndex] : null;
+    let configIndex = chatConfig.configs.findIndex(config => (new RegExp(config.regex)).test(message))
+    
+    if (configIndex !== -1) {
+      console.log(`Replied to \'${message}\' with \'${chatConfig.configs[configIndex]}\'.`);
+      return chatConfig.configs[configIndex];
+    } else {
+      return null;
+    }
   } else {
     return null;
   }
