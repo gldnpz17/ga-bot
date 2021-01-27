e9f1b25201ae89a0013fb41dc571ca28aea7e856
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-01-27 21:08:56 +0700

    Request middleware now calls next.

--- middlewares/request-logger.js
+++ middlewares/request-logger.js
@@ -2,8 +2,9 @@ module.exports.requestLogger = async (req, res, next) => {
   try {
     console.log('Request received.');
     console.log(`URL: ${req.url}`);
-    console.log(`HEADERS: ${req.headers}`);
-    console.log(`BODY: ${req.body}`);
+    console.log(`HEADERS: ${JSON.stringify(req.headers)}`);
+    console.log(`BODY: ${JSON.stringify(req.body)}`);
+    next();
   } catch (err) {
     next(err)
   }
