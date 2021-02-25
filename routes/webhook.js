25bc383766e7997a857f7735d162bf7789045050
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-02-25 13:16:42 +0700

    Forwarded error from webhook.

--- routes/webhook.js
+++ routes/webhook.js
@@ -18,99 +18,103 @@ const { signatureValidator } = require('../middlewares/signature-validator');
 const { requestLogger } = require('../middlewares/request-logger');
 const ApplicationError = require('../common/application-error');
 
-router.post('/', async (req, res) => {
-  req.body.events.map(async (event) => {
-    let groupId = event.source.groupId;
-    if (groupId === null) {
-      lineClient.replyMessage(event.replyToken, {
-        type: 'text',
-        text: 'This bot only works in group chats.'
-      });
-
-      return;
-    }
-    try {
-      if (event.type === 'join') {
-        await configureBotUseCase.initializeConversation(groupId);
-
+router.post('/', async (req, res, next) => {
+  try {
+    req.body.events.map(async (event) => {
+      let groupId = event.source.groupId;
+      if (groupId === null) {
         lineClient.replyMessage(event.replyToken, {
           type: 'text',
-          text: '[Bot initialization complete]\nHello there! o/\n\nType `@BacodBot help` if you need help.'
+          text: 'This bot only works in group chats.'
         });
-      } else if (event.type === 'leave') {
-        configureBotUseCase.removeConversation(groupId);
-      } else if (event.command !== null && event.command !== undefined) {
-        switch (event.command.name) {
-          case 'add-configuration':
-            await configureBotUseCase.addConfiguration(groupId, JSON.parse(event.command.body));
-            
-            lineClient.replyMessage(event.replyToken, {
-              type: 'text',
-              text: 'Configuration saved.'
-            });
-            break;
   
-          case 'list-configurations':
-            let result = await configureBotUseCase.listConfigurations(groupId);
+        return;
+      }
+      try {
+        if (event.type === 'join') {
+          await configureBotUseCase.initializeConversation(groupId);
   
-            lineClient.replyMessage(event.replyToken, {
-              type: 'text',
-              text: JSON.stringify(result, null, 2)
-            });
-            break;
-  
-          case 'remove-configuration':
-            console.log(`Attempting to remove configuration ${event.command.value}`);
-            await configureBotUseCase.removeConfiguration(groupId, event.command.value);
-  
-            lineClient.replyMessage(event.replyToken, {
-              type: 'text',
-              text: 'Configuration removed.'
-            });
-            break;
-
-          case 'help':
-            lineClient.replyMessage(event.replyToken, {
-              type: 'text',
-              text: 'How to use:\nhttps://github.com/gldnpz17/bacod-bot\n\nRegex article:\nhttps://en.wikipedia.org/wiki/Regular_expression'
-            });
-            break;
-
-          default: 
-            lineClient.replyMessage(event.replyToken, {
-              type: 'text',
-              text: 'Command unknown. Type `@BacodBot help` if you need some help.'
-            });
-            break;
-        }
-      } else if (event.type === 'message' && event.message.type === 'text') {
-        let reply = await processMessageUseCase.replyToMessage(groupId, event.message.text);
-
-        console.log(`Received reply: ${reply}. Sending...`);
-        if (reply !== null) {
-          console.log(`Replying to message. replyToken: ${event.replyToken}. reply: ${reply}`);
           lineClient.replyMessage(event.replyToken, {
             type: 'text',
-            text: reply
+            text: '[Bot initialization complete]\nHello there! o/\n\nType `@BacodBot help` if you need help.'
+          });
+        } else if (event.type === 'leave') {
+          configureBotUseCase.removeConversation(groupId);
+        } else if (event.command !== null && event.command !== undefined) {
+          switch (event.command.name) {
+            case 'add-configuration':
+              await configureBotUseCase.addConfiguration(groupId, JSON.parse(event.command.body));
+              
+              lineClient.replyMessage(event.replyToken, {
+                type: 'text',
+                text: 'Configuration saved.'
+              });
+              break;
+    
+            case 'list-configurations':
+              let result = await configureBotUseCase.listConfigurations(groupId);
+    
+              lineClient.replyMessage(event.replyToken, {
+                type: 'text',
+                text: JSON.stringify(result, null, 2)
+              });
+              break;
+    
+            case 'remove-configuration':
+              console.log(`Attempting to remove configuration ${event.command.value}`);
+              await configureBotUseCase.removeConfiguration(groupId, event.command.value);
+    
+              lineClient.replyMessage(event.replyToken, {
+                type: 'text',
+                text: 'Configuration removed.'
+              });
+              break;
+  
+            case 'help':
+              lineClient.replyMessage(event.replyToken, {
+                type: 'text',
+                text: 'How to use:\nhttps://github.com/gldnpz17/bacod-bot\n\nRegex article:\nhttps://en.wikipedia.org/wiki/Regular_expression'
+              });
+              break;
+  
+            default: 
+              lineClient.replyMessage(event.replyToken, {
+                type: 'text',
+                text: 'Command unknown. Type `@BacodBot help` if you need some help.'
+              });
+              break;
+          }
+        } else if (event.type === 'message' && event.message.type === 'text') {
+          let reply = await processMessageUseCase.replyToMessage(groupId, event.message.text);
+  
+          console.log(`Received reply: ${reply}. Sending...`);
+          if (reply !== null) {
+            console.log(`Replying to message. replyToken: ${event.replyToken}. reply: ${reply}`);
+            lineClient.replyMessage(event.replyToken, {
+              type: 'text',
+              text: reply
+            });
+          }
+        }
+  
+        return;
+      } catch(err) {
+        console.log(err.message);
+        console.log(JSON.stringify(err.stack));
+  
+        if (err instanceof ApplicationError) {
+          lineClient.replyMessage(event.replyToken, {
+            type: 'text',
+            text: err.message
           });
         }
+  
+        return;
       }
-
-      return;
-    } catch(err) {
-      console.log(err.message);
-      console.log(JSON.stringify(err.stack));
-
-      if (err instanceof ApplicationError) {
-        lineClient.replyMessage(event.replyToken, {
-          type: 'text',
-          text: err.message
-        });
-      }
-
-      return;
-    }
-  });
+    });
+  } catch(err) {
+    next(err);
+  }
 });
 
 module.exports = router;
