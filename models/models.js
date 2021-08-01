34a6c7909b47cfd03e52a7a4ce82ef6399856a8e
Hafidh R K <72501285+hfrk@users.noreply.github.com>
2021-08-01 09:25:15 +0900

    Implemented un-unsend feature.

--- models/models.js
+++ models/models.js
@@ -29,5 +29,22 @@ const counterProfileSchema = new mongoose.Schema({
   }]
 });
 
+const groupChatMessageHistorySchema = new mongoose.Schema({
+  groupChatId: String,
+  messages: [{
+    id: String,
+    timestamp: Number,
+    userId: String,
+    text: String
+  }],
+  unsentMessages: [{
+    id: String,
+    timestamp: Number,
+    userId: String,
+    text: String
+  }]
+});
+
 module.exports.GroupChatConfig = mongoose.model('GroupChatConfig', groupChatConfigSchema);
 module.exports.CounterProfile = mongoose.model('CounterProfile', counterProfileSchema);
+module.exports.GroupChatMessageHistory = mongoose.model('GroupChatHistory', groupChatMessageHistorySchema);
