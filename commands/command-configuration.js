4906b9bf23fb6aa5da6a954ba7d473c30646a44e
Farrel R <rafirizqy12@gmail.com>
2022-03-04 23:20:24 +0700

    Try isolating the errors (3)
    Try restoring some of the original functionalities

--- commands/command-configuration.js
+++ commands/command-configuration.js
@@ -361,6 +361,8 @@ bot.addFunctionality(event => event.type === 'message' && ['image', 'video', 'au
 
 // Reply to messages.
 bot.addFunctionality((event) => event.type === 'message' && event.message.type === 'text', async (event) => {  
+  await configureUnunsendUseCase.logMessage(event.timestamp, event.source, event.message);
+
   const reply = await processMessageUseCase.replyToMessage(event.source.groupId, event.message.text);
 
   console.log(`Received reply: ${reply.text ?? reply}. Sending...`);
