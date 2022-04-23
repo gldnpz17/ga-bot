1e661333601c94e6fa76a23b99e11628a374dbf5
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2022-04-23 10:50:02 +0700

    I forgot the method was POST.

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -5,7 +5,7 @@ const LoginPage = () => {
     const { key } = event.target
 
     const response = await fetch('/api/login', {
-      method: 'GET',
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
